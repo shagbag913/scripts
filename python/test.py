3fa2fc155135b3b02bbb151a2c9568012db5afdd
shagbag913 <sh4gbag913@gmail.com>
2018-09-03 14:17:07 -0400

    Nuke build{rom,kernel} scripts
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- python/test.py
+++ python/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 
 class colors:
     BLUE = '\033[34m'
@@ -12,12 +13,19 @@ class colors:
     UNDERLINE = '\033[4m'
 
 def error(str):
-    print(colors.RED + len(str) * "-" + "\n" + str + "\n" + len(str) * "-" + colors.RESTORE)
+    print(colors.RED + (len(str) + 6) * '-' + '\n' + 'ERROR: ' + str + '\n' + (len(str)+6) * '-' + colors.RESTORE)
     exit()
 
 def warn(str):
-    print(colors.YELLOW + len(str) * "-" + "\n" + str + "\n" + len(str) * "-" + colors.RESTORE)
+    print(colors.YELLOW + len(str) * '-' + '\n' + str + '\n' + len(str) * '-' + colors.RESTORE)
 
 def info(str):
-    print(colors.BLUE + len(str) * "-" + "\n" + str + "\n" + len(str) * "-" + colors.RESTORE)
+    print(colors.BLUE + len(str) * '-' + '\n' + str + '\n' + len(str) * '-' + colors.RESTORE)
 
+for p in ['lib32-gcc-libs', 'git', 'gnupg', 'flex', 'bison', 'gperf', 'sdl', 'wxgtk2', 'squashfs-tools', 'curl', 'ncurses', 'zlib', 'schedtool',
+			'perl-switch', 'zip', 'unzip', 'libxslt', 'bc', 'rsync', 'lib32-zlib', 'lib32-ncurses', 'lib32-readline', 'lzop', 'pngcrush', 'imagemagick', 'jdk8-openjdk']:
+	os.system('sudo pacman -S --noconfirm ' + p)
+	info('Installed ' + p)
+
+for aur in ['ncurses5-compat-libs', 'lib32-ncurses5-compat-libs', 'xml2']:
+	os.system('git clone https://aur.archlinux.org/' + aur + '.git' ' /tmp/' + aur)
